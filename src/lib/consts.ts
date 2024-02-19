bcdb5016a71197836410d82eedc265c4ac4fcd1d
Xander De Los Santos <xanderdelossantos@Xanders-Air.hsd1.md.comcast.net>
2024-02-19 15:58:06 -0500

    move prices and time to constants

---
+++ src/lib/consts.ts
@@ -0,0 +1,33 @@
+// $
+export const prices = {
+    theatre: {
+        perf: 300,
+        partnerPerf: 250,
+        securityDeposit: 300,
+        techRehearsal: 200,
+        rehearsal: 100
+    },
+    studio: {
+        artist: 40,
+        teachingArtist: 60
+    },
+    addons: {
+        audio: 30,
+        photography: 50,
+        videography: 75,
+        lighting: 75,
+        livestreaming: 75
+    }
+}
+
+// hrs
+export const time = {
+    theatre: {
+        min: 4,
+        max: 20
+    },
+    studio: {
+        min: 1,
+        max: 20
+    }
+}
